20f57523824d93695bc37004a3c3effd6cc85bce
Dhanielgeek <dhanielknightz46@gmail.com>
2026-06-20 13:20:12 -0700

    test new code and db...

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected : ${conn.connection.host}  !!!!`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
